b8b312f1fc1ead839ea1ce1310da6054f1419ea2
Samuel Lopez <samuelelrey0126ytmail.com>
2023-03-11 16:08:39 -0500

    eliminando lineas de codigo inecesarias

--- scripts/mapa.js
+++ scripts/mapa.js
@@ -422,23 +422,9 @@ function selectionsColombia() {
             const posPais = document.createElement("p")
             posPais.textContent = apiPaises[0][0].pais1
 
-            const posPais2 = document.createElement("p")
-            posPais2.textContent = apiPaises[0][0].pais2
-
-            const posPais3 = document.createElement("p")
-            posPais3.textContent = apiPaises[0][0].pais3
-
-            const posPais4 = document.createElement("p")
-            posPais4.textContent = apiPaises[0][0].pais4
-
-            const posPais5 = document.createElement("p")
-            posPais5.textContent = apiPaises[0][0].pais5
 
             paisesList.appendChild(posPais)
-            paisesList.appendChild(posPais2)
-            paisesList.appendChild(posPais3)
-            paisesList.appendChild(posPais4)
-            paisesList.appendChild(posPais5)
+
 
             containerPandPoints.appendChild(paisesList)
 
@@ -1536,24 +1522,9 @@ function selectedf() {
         const posPais = document.createElement("p")
         posPais.textContent = apiPaises[3][0].pais1
 
-        const posPais2 = document.createElement("p")
-        posPais2.textContent = apiPaises[3][0].pais2
-
-        const posPais3 = document.createElement("p")
-        posPais3.textContent = apiPaises[3][0].pais3
-
-        const posPais4 = document.createElement("p")
-        posPais4.textContent = apiPaises[3][0].pais4
-
-        const posPais5 = document.createElement("p")
-        posPais5.textContent = apiPaises[3][0].pais5
 
         paisesList.appendChild(posPais)
-        paisesList.appendChild(posPais2)
-        paisesList.appendChild(posPais3)
-        paisesList.appendChild(posPais4)
-        paisesList.appendChild(posPais5)
-
+        
         containerPandPoints.appendChild(paisesList)
 
 
